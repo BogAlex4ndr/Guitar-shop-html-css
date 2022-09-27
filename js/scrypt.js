c6b61a7b128644b8fd8711f3fd3211f050e47684
BogAlex4ndr <bog.alex4ndr@gmail.com>
2022-09-27 12:22:25 +0300

    guitarShop

---
+++ js/scrypt.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const rewiewSwiper = document.querySelector('.swiper-rewiew');
+
+if (rewiewSwiper) {
+
+const swiper = new Swiper('.swiper-rewiew', {
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      
+    },
+  })};
